960f0c5154f8e619ad8b39c00c2def115a9cb750
c3264515 <49174263+c3264515@users.noreply.github.com>
2019-04-09 11:29:05 +1000

    Updated main.c
    Added getNextWord, readFile and decryptRotation functions

--- main.c
+++ main.c
@@ -2,6 +2,56 @@
 
 int main()
 {
-    
     return 0;
+}
+
+
+
+
+void decryptRotation(char * file, char * encryptionKey)
+{
+	char * word = getNextWord();
+	for (int i = 0; i < 26; i++)
+	{
+        
+	}
+}
+
+
+
+
+
+int getNextWord(char * word)
+{
+	static int index = 0;
+	int wordSize = 0;
+	while (isalpha(dictionary[index])) {
+		wordSize++;
+		index++;
+	}
+
+	for (int i = 0; i < wordSize; i++)
+		word[i] = dictionary[i];
+
+	return wordSize;
+}
+
+int readFile(char * file, char * frequency, char * filePath)
+{
+	FILE * input;
+	input = fopen(filePath, "r");
+	int length = 0;
+	while (!feof(input))
+	{
+		
+		char letter;
+		fscanf(input, "%c", &letter);
+		file[length] = letter;
+		if (letter >= 65 && letter <= 90)
+			frequency[letter-65] ++;
+		else if (letter >= 97 && letter <= 122)
+			frequency[letter-97] ++;
+		length++;
+	}
+	return length;
 }
