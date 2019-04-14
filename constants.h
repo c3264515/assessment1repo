543805ce0c03f63b9e1b8998404cdaadb78b5bfc
c3264515 <49174263+c3264515@users.noreply.github.com>
2019-04-14 13:18:47 +1000

    Created constants.h
    added constants that can be accessed easily without needing to enter into the program code

---
+++ constants.h
@@ -0,0 +1,22 @@
+// File path to dictionary
+#define DICT_FILE_PATH "dictionary.txt"
+
+// Rotation Cipher Key
+#define ROTATION_KEY 1
+
+// Substitution Cipher Key
+#define SUBSTITUTION_KEY "QWERTYUIOPASDFGHJKLZXCVBNM"
+
+
+// Program Mode
+#define MODE 0
+/*
+	Modes:
+	0 - Encrypt Rotation
+	1 - Decrypt Rotation with key
+	2 - Decrypt Rotation without key
+
+	3 - Encrypt Subsitution
+	4 - Decrpyt Substitution with key
+	5 - Decrpyt Substitution without key
+*/ 
